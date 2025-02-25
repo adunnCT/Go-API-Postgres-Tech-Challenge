6b099a826e74abbaea5d2026ad00976ced86dd55
Aaron Dunn <adunn@captechconsulting.com>
2025-02-25 15:18:23 -0500

    initial implementation of remaining user service functions, no testing

--- internal/services/user.go
+++ internal/services/user.go
@@ -26,7 +26,7 @@ func NewUsersService(logger *slog.Logger, db *sql.DB) *UsersService {
 // models.User or an error
 
 func (s *UsersService) Create(ctx context.Context, user models.User) (models.User, error) {
-	s.logger.DebugContext(ctx, "Creating user", "id", user.Name)
+	s.logger.DebugContext(ctx, "Creating user", "name", user.Name)
 
 	res, err := s.db.ExecContext(ctx,
 		`INSERT INTO users (name, email, password) VALUES ($1::text, $2::text, $3::text)`,
@@ -87,17 +87,76 @@ func (s *UsersService) Read(ctx context.Context, id uint64) (models.User, error)
 // updating it to reflect the properties on the provided patch object. A
 // models.User or an error is returned
 func (s *UsersService) Update(ctx context.Context, id uint64, patch models.User) (models.User, error) {
-	return models.User{}, nil
+	s.logger.DebugContext(ctx, "Updating user", "id", id)
+
+	res, err := s.db.ExecContext(
+		ctx,
+		`UPDATE users SET name = $1::text, email = $2::text, password = $3::text WHERE id = $4::int`,
+		patch.Name, patch.Email, patch.Password, id,
+	)
+
+	if err != nil {
+		return models.User{}, fmt.Errorf(
+			"[in services.UsersService.Update] failed to update user: %w",
+			err,
+		)
+	}
+
+	numberOfRowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		return models.User{}, fmt.Errorf(
+			"[in services.UsersService.Update] failed to get number of rows affected: %w",
+			err,
+		)
+	}
+
+	if numberOfRowsAffected == 0 {
+		return models.User{}, fmt.Errorf("[in services.UsersService.Update] no rows were updated")
+	}
+
+	return patch, nil
 }
 
 // Delete attempts to delete the user with the provided id. An error is
 // returned if the delete fails
 func (s *UsersService) Delete(ctx context.Context, id uint64) error {
+	s.logger.DebugContext(ctx, "Deleting user", "id", id)
+
+	_, err := s.db.ExecContext(ctx, `DELETE FROM users WHERE id = $1::int`, id)
+
+	if err != nil {
+		return fmt.Errorf("[in services.UsersService.Delete] failed to delete user: %w", err)
+	}
+
 	return nil
 }
 
 // List attempts to list all users in the database. A slice of models.User
 // or an error is returned
-func (s *UsersService) List(ctx context.Context, id uint64) ([]models.User, error) {
-	return []models.User{}, nil
+func (s *UsersService) List(ctx context.Context, id uint64) (userList []models.User, err error) {
+	s.logger.DebugContext(ctx, "Listing users")
+
+	rows, err := s.db.QueryContext(
+		ctx,
+		`SELECT * FROM users`,
+	)
+
+	if err != nil {
+		return []models.User{}, fmt.Errorf("[in services.UsersService.List] failed to get users: %w", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var user models.User
+
+		if err := rows.Scan(&user.ID, &user.Email, &user.Name, &user.Password); err != nil {
+			return []models.User{}, fmt.Errorf("[in services.UsersService.List] failed to scan row: %w", err)
+		}
+
+		userList = append(userList, user)
+	}
+
+	return
 }
